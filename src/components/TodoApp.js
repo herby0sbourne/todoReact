8753267c12d4c19a1453a3be5326407b20c265c6
Herby <herbyosbourne31@gmail.com>
2022-06-04 12:21:48 -0500

    update to mobile design css and create minins with breakpoint

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -4,7 +4,7 @@ import AddTodo from './AddTodo';
 
 function TodoApp() {
   return (
-    <div className="container">
+    <div>
       <div className="header">
         <h1 className="header-title">Todo</h1>
         <span className="sun-icon"></span>
