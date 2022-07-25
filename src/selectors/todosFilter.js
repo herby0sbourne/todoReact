f832b3c4bf4dd6f340b64552aca4bc5b7a694609
Herby <herbyosbourne31@gmail.com>
2022-07-25 08:19:49 -0500

    update filter to use reselect

--- src/selectors/todosFilter.js
+++ src/selectors/todosFilter.js
@@ -1,3 +1,32 @@
+import { createSelector } from '@reduxjs/toolkit';
+
+const selectTodos = (state) => state.todos;
+const selectFilters = (state) => state.filters;
+
+const todosFilter = createSelector(
+  [selectTodos, selectFilters],
+  (todos, filters) => {
+    const { sortBy } = filters;
+    // console.log(sortBy);
+    return todos.filter((todo) => {
+      if (sortBy === 'all') {
+        return todo.completed === true || todo.completed === false;
+      }
+
+      if (sortBy === 'active') {
+        return todo.completed === false;
+      }
+
+      if (sortBy === 'completed') {
+        return todo.completed === true;
+      }
+      // console.log(todo);
+    });
+  }
+);
+
+/* 
+// Original funtion call
 const todosFilter = (todos, filters) => {
   const { sortBy } = filters;
   // console.log(sortBy);
@@ -15,6 +44,6 @@ const todosFilter = (todos, filters) => {
     }
     // console.log(todo);
   });
-};
+}; */
 
 export default todosFilter;
